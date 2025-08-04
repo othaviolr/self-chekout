7084330ddcbd73c0a50c5217a90d4519d808f196
othav <othav@OTHVIO>
2025-08-04 13:36:48 -0300

    feat: add shadcn initial setup

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">Products Page</h1>
+      <Button>O7havio</Button>
+      <Input placeholder="Teste" />
+    </div>
+  );
+};
+
+export default ProductPage;
